b35cd5c99c3c01621cf213cdd179750cea151079
vedika17bhoite <vedika17bhoite@gmail.com>
2021-05-09 14:48:45 +0530

    trajectory

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gamestate= "onsling"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +69,18 @@ function draw(){
 }
 
 function mouseDragged(){
+    if (gamestate!=="launched")
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate="launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
